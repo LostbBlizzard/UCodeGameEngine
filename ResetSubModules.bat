1978ba1cb37315376ddb74796ac38a054cabdc84
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-07-11 21:24:42 -0400

    added more bat files

---
+++ ResetSubModules.bat
@@ -0,0 +1,56 @@
+cd "Dependencies/Imgui"
+del "Imgui.vcxproj"
+del "Imgui.vcxproj.filters"
+
+cd "../../"
+
+cd "Dependencies/glm"
+del "glm.vcxproj"
+del "glm.vcxproj.filters"
+
+
+cd "../../"
+
+cd "Dependencies/SPIRV-Cross"
+del "SPIRV-Cross.vcxproj"
+del "SPIRV-Cross.vcxproj.filters"
+
+
+cd "../../"
+
+cd "Dependencies/glslang"
+del "glslang.vcxproj"
+del "glslang.vcxproj.filters"
+
+cd "../../"
+
+cd "Dependencies/yaml-cpp"
+del "yaml-cpp.vcxproj"
+del "yaml-cpp.vcxproj.filters"
+
+cd "../../"
+
+cd "Dependencies/box2d"
+del "box2d.vcxproj"
+del "box2d.vcxproj.filters"
+
+cd "../../"
+
+cd "Dependencies/box2d"
+del "box2d.vcxproj"
+del "box2d.vcxproj.filters"
+
+cd "../../"
+
+cd "Dependencies/GLFW"
+del "GLFW.vcxproj"
+del "GLFW.vcxproj.filters"
+
+cd "../../"
+
+cd "Dependencies/plog"
+del "plog.vcxproj"
+del "plog.vcxproj.filters"
+
+
+pause
---
+++ UpdateSubModules.bat
@@ -0,0 +1,4 @@
+git submodule update --remote
+git add .
+git commit -m "git submodule updated"
+git push origin
