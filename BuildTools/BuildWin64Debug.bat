a10914d76b2b3f15781b9e6dd68bbf1738a0ec78
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-07-26 21:36:08 -0400

    git submodule updated

---
+++ BuildTools/BuildWin64Debug.bat
@@ -0,0 +1,3 @@
+
+msbuild ../UCodeGameEngine.sln /t:Build /p:Configuration=Debug /p:Platform=Win64 -maxcpucount
+
---
+++ BuildTools/RunTestsWin64Debug.bat
@@ -0,0 +1,4 @@
+cd ../UCApp
+call ..\Output\UCodeEditor\Win64\Debug\UCodeEditor.exe --RunTests
+
+pause
