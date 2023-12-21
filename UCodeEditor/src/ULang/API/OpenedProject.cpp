e4c8f45b6b630faeeeee743ca0e8ac7d7af2a62e
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2023-12-21 14:23:18 -0800

    updataed ConsoleWindow

--- UCodeEditor/src/ULang/API/OpenedProject.cpp
+++ UCodeEditor/src/ULang/API/OpenedProject.cpp
@@ -8,14 +8,4 @@ UCodeLangExport Path OpenedProject::GetProjectDirPath()
 	return Path();
 }
 
-bool OpenedProject::AsAssetPath(const Path& path, AssetPath& Outpath)
-{
-	UCodeLangThrowException("Not Added");
-	return bool();
-}
-bool OpenedProject::AsAssetDir(const Path& path, AssetDir& Outpath)
-{
-	UCodeLangThrowException("Not Added");
-	return bool();
-}
 UCodeAPIEditorEnd
--- UCodeEditor/src/ULang/API/OpenedProject.hpp
+++ UCodeEditor/src/ULang/API/OpenedProject.hpp
@@ -6,96 +6,21 @@ UCodeAPIEditorStart
 
 
 
-UCodeLangExportSymbol("UCodeGameEngine") struct AssetDir
-{
-	AssetDir()
-	{
-
-	}
-private:
-	Path Base;
-};
 
 
 UCodeLangExportSymbol("UCodeGameEngine") struct AssetPath
 {
-
+public:
+	UCodeLangExport uint64_t ID()
+	{
+		return 0;
+	}
 private:
-	Path Base;
 };
 
 UCodeLangExportSymbol("UCodeGameEngine") struct OpenedProject
 {
-
-	/// <summary>
-	/// Gets the Full Opened Project Path on the disc.
-	/// 
-	/// Example: UCode/Projects/Snake/  
-	/// </summary>
-	/// <returns></returns>
 	UCodeLangExport static Path GetProjectDirPath();
-
-
-	/// <summary>
-	/// Converts a Path into an AssetPath
-	/// May Return nothing if its not the ProjectDir.   
-	/// </summary>
-	/// <returns></returns>
-	UCodeLangExport inline static Optional<AssetPath> AsAssetPath(const Path& path)
-	{
-		AssetPath R;
-		if (AsAssetPath(path,R))
-		{
-			return R;
-		}
-		return {};
-	}
-
-	/// <summary>
-	/// Converts a Path into an AssetDir
-	/// May Return false if its not the ProjectDir.  
-	/// </summary>
-	/// <returns></returns>
-	UCodeLangExport inline static bool AsAssetPath(const Path& path, UCodeLangOutPartype(AssetPath) Outpath)
-	{
-		*Outpath = AssetPath();
-		return  AsAssetPath(path, Outpath);
-	}
-
-	/// <summary>
-	/// Converts a Path into an AssetDir
-	/// May Return false if its not the ProjectDir.  
-	/// </summary>
-	/// <returns></returns>
-	UCodeLangExport static bool AsAssetPath(const Path& path, AssetPath& Outpath);
-
-	UCodeLangExport inline static Optional<AssetDir> AsAssetDir(const Path& path)
-	{
-		AssetDir R;
-		if (AsAssetDir(path, R))
-		{
-			return R;
-		}
-		return {};
-	}
-
-	/// <summary>
-	/// Converts a Path into an AssetDir
-	/// May Return false if its not the ProjectDir.  
-	/// </summary>
-	/// <returns></returns>
-	UCodeLangExport inline static bool AsAssetDir(const Path& path, UCodeLangOutPartype(AssetDir) Outpath)
-	{
-		*Outpath = AssetDir();
-		return AsAssetDir(path, Outpath);
-	}
-
-	/// <summary>
-	/// Converts a Path into an AssetDir
-	/// May Return false if its not the ProjectDir.  
-	/// </summary>
-	/// <returns></returns>
-	UCodeLangExport static bool AsAssetDir(const Path& path, AssetDir& Outpath);
 };
 
 UCodeAPIEditorEnd
