c031d884afa092ed3ef3af2bfcd116d14f1bcc9a
LostbBlizzard <noodproplayz@gmail.com>
2024-02-05 17:35:20 -0800

    fixed GNU_Make.sh

--- GNU_Make.sh
+++ GNU_Make.sh
@@ -1,1 +1,6 @@
-././Dependencies/bin/premake/premake5 gmake2
+if [ "$(uname)" = "Darwin" ];
+then
+./Dependencies/bin/premake/premake5.app gmake2
+else
+./Dependencies/bin/premake/premake5 gmake2
+fi
