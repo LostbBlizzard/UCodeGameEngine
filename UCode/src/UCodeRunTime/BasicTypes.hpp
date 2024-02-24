c15eb132f27f9c5cd25c4e663e8e6d81f6591e6b
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2024-02-24 12:24:41 -0800

    moved FileUpdated To EditorApp

--- UCode/src/UCodeRunTime/BasicTypes.hpp
+++ UCode/src/UCodeRunTime/BasicTypes.hpp
@@ -575,6 +575,36 @@ struct Span
 	{
 		return Span((T*)data, Size);
 	}
+
+	struct Iterator
+	{
+		using iterator_category = std::forward_iterator_tag;
+		using difference_type = std::ptrdiff_t;
+		using value_type = T;
+		using pointer = T*;
+		using reference = T&;
+
+		Iterator(pointer ptr) : m_ptr(ptr) {}
+
+		reference operator*() const { return *m_ptr; }
+		pointer operator->() { return m_ptr; }
+
+		Iterator& operator++() { m_ptr++; return *this; }
+
+		Iterator operator++(int) { Iterator tmp = *this; ++(*this); return tmp; }
+
+		friend bool operator== (const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; };
+		friend bool operator!= (const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; };
+
+	private:
+
+		pointer m_ptr;
+	};
+
+	Iterator begin() { return Iterator(_Data); }
+	Iterator end() { return Iterator(&_Data[_Size]); }
+	const Iterator begin() const { return Iterator(_Data); }
+	const Iterator end() const { return Iterator(&_Data[_Size]); }
 private:
 	T* _Data;
 	size_t _Size;
