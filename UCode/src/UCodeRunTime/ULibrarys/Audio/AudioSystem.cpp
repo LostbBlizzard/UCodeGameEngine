150dd9fb50c5f19aaa856f42d39c67f5dc414ee4
LostbBlizzard <noodproplayz@gmail.com>
2024-04-27 12:11:56 -0700

    added AudioSystem and AudioFile

---
+++ UCode/src/UCodeRunTime/ULibrarys/Audio/AudioSystem.cpp
@@ -0,0 +1,55 @@
+#include "AudioSystem.hpp"
+#include <UCodeRunTime/StaticComponentsIndex.hpp>
+#include "UCodeRunTime/Core/GameRunTime.hpp"
+
+#include "soloud.h"
+#include "soloud_wav.h"
+AudioStart
+
+AudioSystem::AudioSystem(Gamelibrary* lib) : System(lib)
+{
+	soloud = std::make_unique<SoLoud::Soloud>();
+	soloud->init();
+};
+
+void AudioSystem::Play(AudioFile& audio, const AudioPlaySettings& settings)
+{
+	auto handle = (SoLoud::Wav*)audio.GetHandle();
+	soloud->play(*handle);
+}
+
+AudioFile::~AudioFile()
+{
+	if (base)
+	{
+		delete base;
+	}
+}
+void AudioFile::Load(const Path& path) 
+{
+	if (base)
+	{
+		delete base;
+	}
+	base = new SoLoud::Wav();
+	
+	SoLoud::Wav* v = (SoLoud::Wav*)base;
+
+	auto s = path.generic_string();
+	v->load(s.c_str());
+}
+
+AudioSystem* AudioSystem::Get(Gamelibrary* lib)
+{
+	constexpr StaticBooksIndex_t KeyIdex = (StaticBooksIndex_t)StaticBooksIndex::AudioSystem;
+	auto V = lib->Get_StaticComponent(KeyIdex);
+	if (V) { return (AudioSystem*)V; }
+
+	AudioSystem* r = new AudioSystem(lib);;
+
+	lib->MoveSystem(r);
+	lib->SetStaticComponent(KeyIdex, r);
+	return r;
+
+}
+AudioEnd
