d1444513bd90906899de03b5dc78512c87508ccb
LostbBlizzard <106630000+LostbBlizzard@users.noreply.github.com>
2024-10-27 08:52:54 -0400

    updated postmake.lua

--- postmake.lua
+++ postmake.lua
@@ -1,21 +1,30 @@
+function executeorexit(str)
+  exit = os.execute(str)
+
+  if exit == nil then
+    os.exit(1)
+  end
+
+  if not exit == true then
+    os.exit(1)
+  end
+end
+
 local innosetup = postmake.loadplugin("internal/innosetup")
 local shellscript = postmake.loadplugin("internal/shellscript")
 
 local InnoAppID = "{{1CA82BG1-B475-45F7-9FFE-6DF654047519}"
 
 -- App Settings
-postmake.appname = "app"
+postmake.appname = "UCodeGameEngine"
 postmake.appversion = "0.0.1"
-postmake.output = "./output/install"
-
+postmake.apppublisher = "LostbBlizzard"
+postmake.output = "./Output/install"
 postmake.appinstalldir = "~/.ucodegameengine"
+postmake.applicensefile = "LICENSE.txt"
 
 -- Short Hands
 local all = postmake.allconfig
---local unix = postmake.foros("unix")
---local winodows = postmake.foros("winodws")
---local linux = postmake.foros("linux")
---local macos = postmake.foros("macos")
 
 --- Configs
 local win = postmake.newconfig("windows", "x64")
@@ -24,26 +33,49 @@ local mac = postmake.newconfig("macos", "x64")
 
 --- Add Your files
 
-local winsmainprogram = postmake.installdir() .. "./" .. postmake.appname .. ".exe"
+local programdir = postmake.installdir() .. "bin/"
+local unixmainprogram = programdir .. postmake.appname
+local winsmainprogram = unixmainprogram .. ".exe"
 
-win.addfile("main.exe", winsmainprogram)
-gnu.addfile("main", postmake.installdir() .. "./" .. postmake.appname)
-mac.addfile("main_macos", postmake.installdir() .. "./" .. postmake.appname)
+local programbuilddir = "./Output/UCodeEditor/linux64/Debug/"
 
-all.addfile("License.md", postmake.installdir() .. "./License.md")
+win.addxfile(programbuilddir .. "UCodeEditor", winsmainprogram)
+gnu.addxfile(programbuilddir .. "UCodeEditor", unixmainprogram)
+mac.addxfile(programbuilddir .. "UCodeEditor", unixmainprogram)
+
+-- all.addfile(programbuilddir .. "UFiles.data", programdir .. "UFiles.data")
+
+all.addfile(postmake.applicensefile, postmake.installdir() .. postmake.applicensefile)
+
+local addpathflag = all.newflag("Add Path", true)
+
+all.If(addpathflag).addpath(programdir)
+
+executeorexit("cd ./UCodeGameEngineDoc && mdbook build --dest-dir ../Output/UCodeGameEngineDoc")
+all.addfile("Output/UCodeGameEngineDoc/.**", postmake.installdir() .. "doc")
+
+local installwebsite = "https://github.com/LostbBlizzard/UCodeGameEngine/releases/tag/Release-" .. postmake.appversion
 
 ---@type ShellScriptConfig
-local shellsettings = {}
-
-postmake.make(shellscript, { gnu, mac }, shellscript);
+local shellsettings = {
+  weburl = installwebsite,
+  uploaddir = "./Output/upload/",
+}
 
 ---@type InnoSetConfig
 local innosettings = {
-  Appid = InnoAppID,
+  AppId = InnoAppID,
   MyAppURL = "https://github.com/LostbBlizzard/UCodeGameEngine",
   LaunchProgram = winsmainprogram,
-  LicenseFile = "License.md",
-  SetupIconFile = "UCodeGameEngineLogo.ico"
+  SetupIconFile = "UCodeGameEngineLogo.ico",
+  UninstallDelete = {
+    programdir .. "imgui.ini"
+  },
+
 }
 
+-- TODO missing line
+-- Name: "{userdesktop}\{#MyAppName}"; Filename: "{app}\bin\{#MyAppExeName}"; Tasks: desktopicon;
+
+postmake.make(shellscript, { gnu, mac }, shellsettings);
 postmake.make(innosetup, { win }, innosettings);
